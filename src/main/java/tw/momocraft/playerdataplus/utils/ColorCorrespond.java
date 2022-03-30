e0be9830ef8754f429ecd3dfe7c73e83da00510f
momo <momoservertw@gmail.com>
2022-03-30 17:29:27 +0800

    Improved colors.

--- src/main/java/tw/momocraft/playerdataplus/utils/ColorCorrespond.java
+++ src/main/java/tw/momocraft/playerdataplus/utils/ColorCorrespond.java
@@ -15,7 +15,7 @@ public class ColorCorrespond {
     }
 
     private void setUp() {
-        colorMap = CorePlusAPI.getConfig().getConfigPath().getNickColorsMap();
+        colorMap = CorePlusAPI.getConfig().().getNickColorsMap();
         colorList.addAll(colorMap.values());
         String[] colorArray = new String[]{"a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         Collections.addAll(colorList, colorArray);
